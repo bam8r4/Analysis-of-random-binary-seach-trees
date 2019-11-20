9c540bc0d95a7ce052c578bd790d4b24d007e596
Brent Moran <brentmoran@Brents-MacBook-Pro-2.local>
2019-11-20 14:22:27 -0600

    Changes made to main.cpp to put the main section of code in a do-while account.

--- main.cpp
+++ main.cpp
@@ -11,37 +11,39 @@ int main()
   int t = 1; //This value after cin will represent the number of trees that we will create with n nodes.
   int sum = 0; //This value will store the heights of our trees. Then will be used to average the values.
 
+  do{
+      cout<<"\nInput how many trees should be formed: ";
+      cin>>t;
+      cout<<"\nInput what the size of each tree should be: ";
+      cin>>n;
 
-  cout<<"\nInput how many trees should be formed: ";
-  cin>>t;
-  cout<<"\nInput what the size of each tree should be: ";
-  cin>>n;
+      int height[t];
 
-  int height[t];
+      //This will build our binary search tree.
+      for(int i = 0; i < t; i++)
+      {
 
-  //This will build our binary search tree.
-  for(int i = 0; i < t; i++)
-  {
+        binaryTree tree;    //Each run through the loop we are making a new binary tree.
 
-    binaryTree tree;    //Each run through the loop we are making a new binary tree.
-
-    for(int j = 0; j < n; j++)
-    {
-      tree.insertNode(rand()%10000);    //We are inserting n random integer nodes in the tree.
-    }
-    height[i] = tree.getMaxHeight();    //Here is where we will return an integer value that represents our height.
-  }
+        for(int j = 0; j < n; j++)
+        {
+          tree.insertNode(rand()%10000);    //We are inserting n random integer nodes in the tree.
+        }
+        height[i] = tree.getMaxHeight();    //Here is where we will return an integer value that represents our height.
+      }
 
 
 
-  cout<<endl;
-  for(int i = 0; i < t; i++)
-  {
-    cout<<"The height of tree "<<i+1<<" is "<<height[i]<<endl;
-    sum += height[i];
-  }
+      cout<<endl;
+      for(int i = 0; i < t; i++)
+      {
+        //cout<<"The height of tree "<<i+1<<" is "<<height[i]<<endl;
+        sum += height[i];
+      }
 
-  cout<<"The average height of "<<t<<" trees with "<<n<<" nodes is: "<<sum/t<<endl;
-  cout<<endl;
+      cout<<"The average height of "<<t<<" trees with "<<n<<" nodes is: "<<sum/t<<endl;
+      cout<<endl;
+      sum = 0;
+    }while(t>0 && n>0);
 
 }
